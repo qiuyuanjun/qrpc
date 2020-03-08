a2412e0ada437d77308ecee845e93fbbceefb8ed
qiuyuanjun <18707097515@163.com>
2020-03-08 17:59:27 +0800

    调整优化ServiceRegistrar

--- qrpc-server/src/main/java/com/qiuyj/qrpc/service/DefaultServiceDescriptorContainer.java
+++ qrpc-server/src/main/java/com/qiuyj/qrpc/service/DefaultServiceDescriptorContainer.java
@@ -9,8 +9,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
